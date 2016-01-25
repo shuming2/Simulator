0212832515045642c1383bbfdfa9d71bd563189c
shuming2 <Amber@uws135705usr.uws.ualberta.ca>
2016-01-25 16:47:28 -0700

    Initial Commit

---
+++ Simulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Simulator
+//
+//  Created by ZhangShuming on 16/1/25.
+//  Copyright © 2016年 ZhangShuming. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
