31ec603f4d47267fe128b2323483c562bd235291
shuming2 <Amber@uws135705usr.uws.ualberta.ca>
2016-01-25 16:57:39 -0700

    header file

---
+++ Simulator/Simulator.h
@@ -0,0 +1,13 @@
+//
+//  simulator.h
+//  Simulator
+//
+//  Created by ZhangShuming on 16/1/25.
+//  Copyright © 2016年 ZhangShuming. All rights reserved.
+//
+
+#ifndef simulator_h
+#define simulator_h
+
+
+#endif /* simulator_h */
